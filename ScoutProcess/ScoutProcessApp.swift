d5fe43d6407c9feebad9da9c3c0e8cd966e85843
Brian Bennett <bennettb15@gmail.com>
2026-02-28 21:02:58 -0500

    Initial Commit

---
+++ ScoutProcess/ScoutProcessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutProcessApp.swift
+//  ScoutProcess
+//
+//  Created by Brian Bennett on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoutProcessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
